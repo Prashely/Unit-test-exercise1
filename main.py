cdf0094002ad6123bbbf6f3a84c95a0620c05a05
Yamkela Fukuse <yafukjhb025@student.wethinkcode.co.za>
2025-10-01 10:23:55 +0200

    done

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 def factorial(n):
-    """Calculates the factorial of a non-negative integer."""
-    # TODO: Implement the logic
-    pass 
+    fact = 1
+    while n != 0:
+        fact = fact * n
+        n -= 1
+    return fact
+
+# print(factorial(0))
 
 def fibonacci(n):
-    """Returns the nth number in the Fibonacci sequence."""
-    # TODO: Implement the logic
-    return None
+    fib_list = [0,1]
+    for i in range(1,n):
+        new_item = fib_list[i] + fib_list[i-1]
+        fib_list.append(new_item)
+        print(f"new item:{new_item}, fib_list[i]:{fib_list[i]}, fib_list[i-1]:{fib_list[i-1]}, {fib_list}, thid is I cureently {i}")
+
+    return fib_list[n]
+
+print(fibonacci(4))
 
 def fizzbuzz(n):
-    """
-    Returns a list for the FizzBuzz game up to n.
-    - Multiples of 3 are "Fizz"
-    - Multiples of 5 are "Buzz"
-    - Multiples of both 3 and 5 are "FizzBuzz"
-    """
-    # TODO: Implement the logic
-    return []
+
+    fizzbuzz_list = []
+    for i in range(1,n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            fizzbuzz_list.append("FizzBuzz")
+        elif i % 3 == 0:
+            fizzbuzz_list.append("Fizz")
+        elif i % 5 == 0:
+            fizzbuzz_list.append("Buzz")
+        else:
+            fizzbuzz_list.append(i)
+
+
+    return fizzbuzz_list
+
+# print(fizzbuzz(15))
